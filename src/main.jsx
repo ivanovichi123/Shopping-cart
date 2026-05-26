07cf7f2dc8227c0496e0594b80c24e867c528da1
ivanovichi <ivan.arthur18@gmail.com>
2026-05-26 08:40:32 -0600

    feat: changes to use the browser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './components/App'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import routes from './components/Routes'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </StrictMode>,
 )
