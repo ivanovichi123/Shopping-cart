24b843e42e919436163f7f4f85fe2e59ca14022e
ivanovichi <ivan.arthur18@gmail.com>
2026-05-26 08:41:27 -0600

    feat: Changes made for the router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import Cart from "../assets/cart.svg"
 import styles from "../styles/navBar.module.css"
+import { Link } from "react-router"
 
 const NavBar = () => {
     return (
         <nav className={ styles.navigation }>
-            <p className={ styles.Home }>Home</p>
+            <Link to = "home" className={ styles.Home }>Home</Link>
             <p className={ styles.Shop }>Shop</p>
             <img className={ styles.Cart } src={ Cart } alt="A cart" />
         </nav>
