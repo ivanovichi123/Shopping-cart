7bb54c606843f2b5772a9004f750e9c87dbb263e
ivanovichi <ivan.arthur18@gmail.com>
2026-05-26 08:42:13 -0600

    feat: Pages necessaries for the page route

---
+++ src/components/Routes.jsx
@@ -0,0 +1,20 @@
+import { Navigate, replace } from "react-router";
+import { App } from "./App";
+import { ErrorPage } from "./ErrorPage";
+
+const routes = [
+    {
+        path: "/",
+        element: <Navigate to="/home" replace/>
+    },
+    {
+        path: "/home",
+        element: <App />
+    },
+    {
+        path: "*",
+        element: <ErrorPage />
+    }
+]
+
+export default routes
