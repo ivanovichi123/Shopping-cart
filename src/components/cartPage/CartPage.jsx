7e28c728cf1e0adc20e1e1063c62d240fcec8776
ivanovichi <ivan.arthur18@gmail.com>
2026-06-02 14:31:26 -0600

    feat: Added template for the cart page

--- src/components/cartPage/CartPage.jsx
+++ src/components/cartPage/CartPage.jsx
@@ -3,21 +3,32 @@ import { Footer } from "../Footer";
 import { useState } from "react";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
+import styles from "../../styles/cartPage.module.css";
+import Delete from "../../assets/Delete.svg"
 
 const CartPage = () => {
   const { cartNumber, SetCarNumber } = useContext(CartContext);
 
   return (
-    <>
-      <header>
+    <div className={styles.pageContainer}>
+      <header className={styles.theHeader}>
         <h1>Cart</h1>
       </header>
       <NavBar cartItems={cartNumber}/>
-      <main>
-        <p>Hello</p>
+      <main className={styles.theMain}>
+        <div className={styles.theContainer}>
+          <p className={styles.theItem}>Item:</p>
+          <p className={styles.theItemName}>Apple</p>
+          <div className={styles.theChanges}>
+            <button className={styles.theMore}>+</button>
+            <button className={styles.theInput}>0</button>
+            <button className={styles.theLess}>-</button>
+          </div>
+          <img src={Delete} alt="A trash Can" className={styles.theDelete}/>
+        </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
