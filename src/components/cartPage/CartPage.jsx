9824c34e5a0af1b4fb42eb7fea40f28a25d90554
ivanovichi <ivan.arthur18@gmail.com>
2026-06-02 08:06:34 -0600

    feat: Create component for the cart

---
+++ src/components/cartPage/CartPage.jsx
@@ -0,0 +1,18 @@
+import { NavBar } from "../NavBar";
+import { Footer } from "../Footer";
+import { useState } from "react";
+
+const CartPage = () => {
+    return (
+        <>
+            <header>
+                <h1>Cart</h1>
+            </header>
+            <NavBar />
+            <main>
+                <p>Hello</p>
+            </main>
+            <Footer />
+        </>
+    )
+}
